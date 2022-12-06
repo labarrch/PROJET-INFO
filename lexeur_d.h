54dfd343ecfc719639ed69b0b2ea94dfdf014abc
labarrch <charleslabarre91@gmail.com>
2022-12-06 04:13:43 -0500

    second commit

---
+++ lexeur_d.h
@@ -0,0 +1,8 @@
+int lineProcess(string line, vector<string>& list);
+int lineProcessJson(string line, vector<string>& list);
+int characAlpha(string charac);
+int characSymb(string charac);
+int characAlphaJson(string charac);
+int characSymbJson(string charac);
+
+int lexeur(vector<string>& lChainDot, vector<string>& lChainJson);
